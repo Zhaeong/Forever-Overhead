df158b4e13b65775e4eaac2de3a191ae835bff59
Zhaeong <owen_zzz@hotmail.com>
2016-01-15 23:16:20 -0500

    added camera controls and player movement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,26 +4,29 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
     public GameObject PlayerChar;
+    public float cameraHeight = 2;
+    public float cameraDistance = 5;
     private Vector3 cameraOffset;
 	// Use this for initialization
 	void Start () {
+        Vector3 playerPosit = PlayerChar.transform.position;
+        float cameraX = PlayerChar.transform.position.x;
+        float cameraY = PlayerChar.transform.position.y + 5;
+        float cameraZ = PlayerChar.transform.position.z - 5;
 
-
+        transform.position = new Vector3(cameraX, cameraY, cameraZ);
+        transform.LookAt(playerPosit);
+       
+        Debug.DrawLine(playerPosit, cameraOffset, new Color(255, 0, 0));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 playerPosit = PlayerChar.transform.position;
-
-        float cameraX = PlayerChar.transform.position.x + 5;
-        float cameraY = PlayerChar.transform.position.y + 5;
-        float cameraZ = PlayerChar.transform.position.z + 5;
-        Vector3 cameraOffset = new Vector3(cameraX, cameraY, cameraZ);
-
-        Debug.DrawLine(playerPosit, cameraOffset, new Color(255, 0, 0));
-
-        transform.position = cameraOffset;
-        transform.LookAt(playerPosit);
-	
-	}
+        float rotationAngle = PlayerChar.transform.eulerAngles.y;
+        float height = PlayerChar.transform.position.y + cameraHeight;
+        Quaternion qRotation = Quaternion.Euler(0, rotationAngle, 0);
+        transform.position = new Vector3(PlayerChar.transform.position.x, height, PlayerChar.transform.position.z);
+        transform.position -= qRotation * Vector3.forward * cameraDistance;
+        transform.LookAt(PlayerChar.transform);
+    }
 }
